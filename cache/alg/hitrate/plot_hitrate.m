7645f22e0027b39886d04cfdc64bf55f4fd5ddce
Artemy Kolesnikov <artemy.kolesnikov@gmail.com>
2014-11-08 20:31:54 +0300

    Cache hit rate plot

--- cache/alg/hitrate/plot_hitrate.m
+++ cache/alg/hitrate/plot_hitrate.m
@@ -10,18 +10,25 @@ mq = load("mq.txt")(:, 2);
 mq_wo_exp = load("mq_wo_exp.txt")(:, 2);
 s4lru = load("s4lru.txt")(:, 2);
 
-plot(x, tq, '-.', 'linewidth', 2,
-     x, arc, '-.', 'linewidth', 2,
-     x, fifo, '-.', 'linewidth', 2,
-     x, lfu, ':', 'linewidth', 2,
-     x,lru, 'linewidth', 2,
+plot(x, mq_wo_exp, 'linewidth', 2,
      x, mid, 'linewidth', 2,
+     x, s4lru, 'linewidth', 2,
+     x, lfu, 'linewidth', 2,
+     x, arc, 'linewidth', 2,
+     x, tq, 'linewidth', 2,
      x, mq, 'linewidth', 2,
-     x, mq_wo_exp, 'linewidth', 2,
-     x, s4lru, 'linewidth', 2);
+     x, lru, '-.', 'linewidth', 2,
+     x, fifo, '-.', 'linewidth', 2);
 
-l = legend('2Q','ARC','FIFO','LFU','LRU','MID',
-       'MQ','MQ wo expiration', 'S4LRU',
+l = legend('MQ*','Mid LRU','S4LRU','LFU','ARC','2Q',
+       'MQ','LRU', 'FIFO',
        'location', 'east');
  
 set(l, 'fontsize', 14);
+
+grid on;
+
+xlabel('Cache size in item number');
+ylabel('Hit rate in %');
+
+print('-dpng', '-color', 'hitrate.png');
